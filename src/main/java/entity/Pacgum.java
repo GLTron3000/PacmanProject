2796090960aff7d77f0db01f5744a46015801299
marc <march13821@aol.com>
2019-11-16 17:55:11 +0100

    récupération d'une pacgum + test

--- src/main/java/entity/Pacgum.java
+++ src/main/java/entity/Pacgum.java
@@ -1,11 +1,15 @@
 package entity;
 
+import JSON.LevelData;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
 
 
 public class Pacgum extends Pickable{
 
+    LevelData data = new LevelData();
     public Pacgum(double x, double y) {
         super(x, y);
         type="Pacgum";
@@ -13,7 +17,19 @@ public class Pacgum extends Pickable{
 
     @Override
     public void onPick() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        data.load("customLevel1.pml");
+        int i = 0;
+        int test = 0;
+        while(test == 0)
+        {
+            if(data.pickables.get(i).x == this.x && data.pickables.get(i).y == this.y) {
+                data.pickables.remove(i);
+                test = 1;
+            }
+            else
+                i++;
+        }
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
---
+++ src/test/java/entity/PacgumTest.java
@@ -0,0 +1,21 @@
+package entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PacgumTest {
+
+    @Test
+    void onPick() {
+        Pacgum pacgum = new Pacgum(25.0,25.0);
+        pacgum.data.load("customLevel2.pml");
+        int test = pacgum.data.pickables.size();
+        pacgum.onPick();
+        assertNotEquals(test,pacgum.data.pickables.size());
+    }
+
+    @Test
+    void draw() {
+    }
+}
