34c50d6915bd12150b71cbbc2eb3d35a46d2ff10
eckterolite <apupier3@gmail.com>
2019-11-19 16:36:05 +0100

    Collision behavior update -BugFix-

--- src/main/java/game/CollisionEngine/CollideBehaviorClassic.java
+++ src/main/java/game/CollisionEngine/CollideBehaviorClassic.java
@@ -1,6 +1,7 @@
 package game.CollisionEngine;
 
 import entity.Direction;
+import entity.Entity;
 import entity.Movable;
 import entity.Wall;
 
@@ -8,16 +9,16 @@ public class CollideBehaviorClassic implements CollideBehavior {
     @Override
     public void collideMovableWall(Movable m, Wall w ){
 
-        if(m.direction== Direction.UP && (w.getY())-m.getY()<=-24.5 && Math.abs((w.getX())-m.getX())!=24.5){
+        if(m.direction== Direction.UP && ((w.getY())-m.getY()>=-24.5 && Math.abs((w.getX())-m.getX())!=24.5)){
             System.out.println("Colli : UP");
             m.stop();
-        }else if(m.direction==Direction.LEFT && (w.getX())-m.getX()<=-24.5 && Math.abs((w.getY())-m.getY())!=24.5 ){
+        }else if(m.direction==Direction.LEFT && ((w.getX())-m.getX()>=-24.5 && Math.abs((w.getY())-m.getY())!=24.5)){
             System.out.println("Colli : left : w :"+w.getX()+"m : "+m.getX() );
             m.stop();
-        }else if(m.direction==Direction.DOWN && (w.getY())-m.getY()>=24.5 && Math.abs((w.getX())-m.getX())!=24.5){
+        }else if(m.direction==Direction.DOWN && ((w.getY())-m.getY()<=24.5 && Math.abs((w.getX())-m.getX())!=24.5)){
             System.out.println("Colli : Down");
             m.stop();
-        }else if(m.direction==Direction.RIGHT && (w.getX())-m.getX()>=24.5 && Math.abs((w.getY())-m.getY())!=24.5){
+        }else if(m.direction==Direction.RIGHT && ((w.getX())-m.getX()<=24.5 && Math.abs((w.getY())-m.getY())!=24.5)){
             System.out.println("Colli : Right");
             m.stop();
         }
