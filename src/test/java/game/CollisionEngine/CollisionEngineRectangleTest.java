2b66529869d4d8b6973881e4c0a5af73a33b7b78
eckterolite <apupier3@gmail.com>
2019-11-19 17:17:47 +0100

    CollisionEngine BugFix

---
+++ src/test/java/game/CollisionEngine/CollisionEngineRectangleTest.java
@@ -0,0 +1,12 @@
+package game.CollisionEngine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollisionEngineRectangleTest {
+
+    @Test
+    void isCollide() {
+    }
+}
---
+++ src/test/java/game/CollisionEngine/OutOfBoardWrapperTest.java
@@ -0,0 +1,14 @@
+package game.CollisionEngine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutOfBoardWrapperTest {
+
+    @Test
+    void behavior() {
+        OutOfBoardBehavior oob= new OutOfBoardWrapper(800,400);
+
+    }
+}
